379040e932760a48c512e238ba48df38007b545e
bill omole <omolebill01@gmail.com>
2023-05-14 16:44:32 +0200

    Tuple instead of tuple

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ We impliment an index function """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int] :
+def index_range(page: int, page_size: int) -> Tuple[int, int] :
     """ We impliment an indexing function to find the 
         start and end of a page
     """
