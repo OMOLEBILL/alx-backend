ef72abad4ce95f6e8a3016e1ff38cca8e289bfc3
bill omole <omolebill01@gmail.com>
2023-05-14 16:45:52 +0200

    pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,8 +3,8 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int] :
-    """ We impliment an indexing function to find the 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ We impliment an indexing function to find the
         start and end of a page
     """
     start = (page - 1) * page_size
