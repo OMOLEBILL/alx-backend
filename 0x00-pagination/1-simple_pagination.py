1e8281fb8e8ef2d661c8f0165b27db4a17dfe4f6
bill omole <omolebill01@gmail.com>
2023-05-14 20:33:30 +0200

    1-simple_pagination.py

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ We impliment an indexing function to find the
+        start and end of a page
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ We get a page """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        lists = self.dataset()
+        if start > len(lists):
+            return []
+        return lists[start:end]
