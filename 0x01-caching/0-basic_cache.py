1d6298cc2fafeb86557b02dad11a6714e304c368
bill omole <omolebill01@gmail.com>
2023-05-16 23:31:25 +0200

    added

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
 
-""" In this module we impliment the get and put methods
+"""
+Module implements BasicCache
 """
 
-
-BaseCaching = __import__("BaseCaching").BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ We inherit from the base class and modify with the put and get methods
     """
-
+    Class implements BasicCache
+    """
     def put(self, key, item):
-        """ We overide the base method from the base class to put some data
-        args:
-            Key : the key to update from the dictinary
-            item : the value of the key
-        return:
-            Nonething but update the self.cache_data
-        """
+        """Method assigns values to cache"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ we retrieve the value of the given key if it's not None and
-            it exist in the cache dictionary
-        args:
-            Key: the key to retrive
-        return:
-            the value of teh given key
-        """
+        """Method retrieves value from cache"""
         return self.cache_data.get(key)
