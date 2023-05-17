d103631043770825129e64ce11789cec2c19bf21
bill omole <omolebill01@gmail.com>
2023-05-17 20:30:52 +0200

    2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+""" In this module we impliment the get and put methods
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ We inherit from the base class and modify with the put and get methods
+    """
+    def __init__(self):
+        """ we initialize the FiFOCache class and super the base class """
+        super().__init__()
+        self.key = []
+
+    def put(self, key, item):
+        """ We overide the base method from the base class to put some data
+            if the number of items in self.cache_data is higher than
+            BaseCaching.Max_items:
+                 we discard the Last item and print discard with the key
+        args:
+            Key : the key to update from the dictinary
+            item : the value of the key
+        return:
+            Nonething but update the self.cache_data
+        """
+        if key and item:
+            if key in self.key:
+                self.key.remove(key)
+            self.key.append(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            index = self.key.pop(len(self.key) - 2)
+            del self.cache_data[index]
+            print("DISCARD: {}".format(index))
+
+    def get(self, key):
+        """ we retrieve the value of the given key if it's not None and
+            it exist in the cache dictionary
+        args:
+            Key: the key to retrive
+        return:
+            the value of teh given key
+        """
+        return self.cache_data.get(key)
